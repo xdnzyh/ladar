7e899ffadcf6858002c54cc3305c2aba8da28d51
xdnzyh <zyh2147483647@gmail.com>
2026-09-10 11:49:40 +0800

    Install resilient sync policy before hardware UI starts

--- hardware_app.py
+++ hardware_app.py
@@ -1,6 +1,9 @@
+from sync_resilience import install_for_hardware_runtime
+
+install_for_hardware_runtime()
+
 from navigation_app import run_app
 
 
 if __name__ == "__main__":
     run_app("hardware")
-
